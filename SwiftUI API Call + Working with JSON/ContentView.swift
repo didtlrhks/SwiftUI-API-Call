e367809997acccf09e39ab09ad743b80f5175c4a
sikwan <yysskk99999@naver.com>
2024-01-24 20:22:05 +0900

    api call setting

--- SwiftUI API Call + Working with JSON/ContentView.swift
+++ SwiftUI API Call + Working with JSON/ContentView.swift
@@ -7,15 +7,53 @@
 
 import SwiftUI
 
+struct Course: Hashable, Codable {
+    let name : String
+    let image: String
+    
+}
+
+
+class ViewModel : ObservableObject {
+    @Published var courses : [Course] = []
+    
+    func fetch() {
+        guard let url = URL(string:
+                                "https://iosacademy.io/api/v1/courses/index.php") else {
+            return
+        }
+        let task = URLSession.shared.dataTask(with: url) { [weak self]
+            data,_,error in
+            guard let data = data, error == nil else {
+                return
+            }
+            
+            //TODO: - convert api
+            
+            do {
+                let courses = try JSONDecoder().decode([Course].self,from: data)
+                DispatchQueue.main.async {
+                    self?.courses = courses
+                }
+            }catch {
+                print(error)
+            }
+        }
+    }
+}
+
+
+
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            List {
+                
+              Text("df")
+                
+            }.navigationTitle("Courses")
         }
-        .padding()
     }
 }
 
