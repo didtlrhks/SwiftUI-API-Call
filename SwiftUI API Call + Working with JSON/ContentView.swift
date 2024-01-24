27035488871838aeb77f204984ccbf3753c84bc2
sikwan <yysskk99999@naver.com>
2024-01-24 20:05:30 +0900

    init

---
+++ SwiftUI API Call + Working with JSON/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI API Call + Working with JSON
+//
+//  Created by 양시관 on 1/24/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
